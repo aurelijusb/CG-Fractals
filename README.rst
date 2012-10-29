b41dedfa6697379648a79da750179f83fbad3e25
Aurelijus Banelis <aurelijus@banelis.lt>
2012-10-29 20:58:39 +0200

    Affine and random fractals. Colorization of fractals

--- README.rst
+++ README.rst
@@ -1,7 +1,21 @@
 Fractals example
 ================
 
-Fractals created using affine transformations.
+Functionality
+---------------
+
+ * Draw fractals using affine transformation
+ * Animate interations (1 big image to meny pixel size images)
+ * Random based (chaos game) fractal painting
+ * Different colors for each transformation
+
+
+Usage
+-----
+
+ * [Enter] - swith between random/transformation based drawing.
+ * [Space] - iterate affine transformations
+
 
 Transformations
 ---------------
@@ -19,12 +33,13 @@ Contractions (by output):
 
 Contractions (attributes):
 
-     T1 = (0.5, 1,  180, 180,  0.5, 1)
-     T2 = (-0.5, 0.5,  90, 90,  0.5, 0.5)
-     T3 = (0.5, 0.5,  180, 180,  1, 1)
-     T4 = (0.5, 0.5,  0, 0,  0.5, 0)
+     T1 = (0.25, 0.25, 180, 180,  0.5,   1)
+     T2 = (-0.5,  0.5,  90,  90,  0.5, 0.5)
+     T3 = ( 0.5,  0.5, 180, 180,    1,   1)
+     T4 = (0.5, 0.5,     0,   0,  0.5,   0)
 
 P_(k+1) = T1(P_k) U .. U T4(P_k)
+
 
 Compilation
 -----------
@@ -32,8 +47,18 @@ Compilation
 Libraries
  * OpenGl
  * Free GLUT
+ * Math
 
 To compile program::
 
-    gcc src/main.c -lGL -lGLU -lglut -o U2Aurelijus
+    gcc src/main.c -lGL -lGLU -lglut -lm -o U2Aurelijus
   
+  
+References
+----------
+
+ * http://uosis.mif.vu.lt/~rimask/destymas/kompiuterine-grafika-ps/2-uzd/
+ * http://classes.yale.edu/fractals/
+ * http://chaos4.phy.ohiou.edu/~thomas/fractal/game.html
+ * A. Lenkevičius, J. Matickas „KOMPIUTERINĖ GRAFIKA“. Technologija, Kaunas, 2004.
+ * http://www.eecis.udel.edu/~chandra/640/Spring05/lecture4.ppt
--- src/main.c
+++ src/main.c
@@ -7,15 +7,43 @@
 
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <math.h>
+
 #define KEY_ESCAPE 27
+#define KEY_ENTER 13
+#define PI 3.14159265
 
+/*
+ * Contractions
+ */
+#define N 4
+GLfloat scale[N][2]     = {{0.25,  0.25}, {-0.5, 0.5}, {0.5, 0.5}, {0.5, 0.5}};
+GLfloat rotation[N]     = {          180,          90,        180,          0};
+GLfloat translate[N][2] = {{0.5,      1}, { 0.5, 0.5}, {  1,   1}, {0.5,   0}};
+GLfloat colours[N][3]   = {{0.5, 1, 0.5}, {0, 1,   1}, {1,0,   1}, {1,1,   0}};
+
+
+/*
+ * Global variables
+ */
 int currentDepth = 0;
+enum MODE {transformationBased, randomBased} currentMode = randomBased;
 
+/*
+ * Interaction
+ */
 void onKeyPress(unsigned char key, int keyX, int keyY) {
     switch (key) {
         case KEY_ESCAPE:
         case 'q':
             exit(1);
+        break;
+        case KEY_ENTER:
+            if (currentMode == randomBased) {
+                currentMode = transformationBased;
+            } else {
+                currentMode = randomBased;
+            }
         break;
         case ' ':
             if (currentDepth < 9) {
@@ -28,6 +56,10 @@ void onKeyPress(unsigned char key, int keyX, int keyY) {
     glutPostRedisplay();
 }
 
+
+/*
+ * Fractal from single element
+ */
 void drawPicture() {
     glBegin(GL_QUADS);
         glVertex3f( 0.3, 0.2, 0.0 );
@@ -42,12 +74,12 @@ void drawPicture() {
     glEnd();
 }
 
-void drawFractal(int depth) {
-    switch(depth) {
-    case 0:
-        drawPicture();
-        break;
-    default:
+
+/*
+ * Drawing fractal using affine transforamtions
+ */
+void drawFractalAffine(int depth) {
+    if (depth > 0) {
         /* Parts of fractal:    T1 T2
          *                      T3 T2
          * 
@@ -57,41 +89,75 @@ void drawFractal(int depth) {
          * 
          * Order convention: T(R(S(v)))
          */
-        
-        glColor3f( 0.5, 1, 0.5 );  //T1
-        glPushMatrix();
-            glTranslatef(0.5, 1, 0);
-            glRotatef(180, 0, 0, 1);
-            glScalef(0.25, 0.25, 1);
-            drawFractal(depth - 1);
-        glPopMatrix();
-                
-        glColor3f( 0.0, 1.0, 1.0 );	//T2
-        glPushMatrix();
-            glTranslatef(0.5, 0.5, 1);
-            glRotatef(90, 0, 0, 1);
-            glScalef(-0.5, 0.5, 1);
-            drawFractal(depth - 1);
-        glPopMatrix();
-        
-        glColor3f( 1.0, 1.0, 0.0 );	//T3
-        glPushMatrix();
-            glTranslatef(1, 1, 0);
-            glRotatef(180, 0, 0, 1);
-            glScalef(0.5, 0.5, 1);
-            drawFractal(depth - 1);
-        glPopMatrix();
-        
-        glColor3f( 1.0, 0.0, 1.0 );	//T4
-        glPushMatrix();
-            glTranslatef(0.5, 0, 0);
-            glScalef(0.5, 0.5, 1);
-            drawFractal(depth - 1);
-        glPopMatrix();
-        break;
+
+        int i;
+        for (i = 0; i < N; i++) {
+            glPushMatrix();
+                glTranslatef(translate[i][0], translate[i][1], 0);
+                glRotatef(rotation[i], 0, 0, 1);
+                glScalef(scale[i][0], scale[i][1], 1);
+                glColor3f(colours[i][0], colours[i][1], colours[i][2]);
+                drawFractalAffine(depth - 1);
+            glPopMatrix();
+        }
+    } else {
+        drawPicture();
     }
 }
 
+
+/*
+ * Drawing fractal using chaos game
+ */
+void drawFractalRandom() {
+    GLfloat p[2] = {0.5,0.5};
+    int max = 50000;
+    int i, j;
+    for (i = 0; i < max; i++) {
+        j = rand()%4;
+        
+        
+        /*     / x'\   / s_x*cos(r)  -s_x*sin(r)  t_x \   / x \
+         * p = | y'| = | s_x*sin(r)   s_y*cos(r)  t_y | x | y |
+         *     \ 1 /   \     0             0       1  /   \ 1 /
+         */
+        float x = p[0];
+        float y = p[1];
+        float r = rotation[j] * PI / 180;
+        float Sx = scale[j][0];
+        float Sy = scale[j][1];
+        float Tx = translate[j][0];
+        float Ty = translate[j][1];
+        p[0] = ( x * Sx * cos(r) ) + (-y * Sy * sin(r)) + Tx;
+        p[1] = ( x * Sx * sin(r) ) + ( y * Sy * cos(r)) + Ty;
+        
+        glColor3f(colours[j][0], colours[j][1], colours[j][2]);
+        glBegin(GL_POINTS);
+            glVertex2fv(p); 
+        glEnd();
+    }
+}
+
+
+/*
+ * Main drawing loop
+ */
+void renderScene(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    if (currentMode == randomBased) {
+        drawFractalRandom();
+    } else {
+        drawFractalAffine(currentDepth);    
+    }
+    
+    glutSwapBuffers();
+}
+
+
+/*
+ * Initial view configurations
+ */
 void init() {
     glMatrixMode(GL_PROJECTION);
     glTranslatef(-1, -1, 0);
@@ -99,14 +165,10 @@ void init() {
     glMatrixMode(GL_MODELVIEW);
 }
 
-void renderScene(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    drawFractal(currentDepth);
-    
-    glutSwapBuffers();
-}
-
+/*
+ * Initiating OpenGl
+ */
 int main(int argc, char **argv) {
     // Init GLUT and create Window
     glutInit(&argc, argv);
